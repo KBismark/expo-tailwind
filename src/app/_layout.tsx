a8ffc17601a60857933c12f94efe866e3c6be728
Bismark Yamoah <81543123+KBismark@users.noreply.github.com>
2024-12-31 15:56:13 +0000

    Update _layout.tsx

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,7 +3,7 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import React, { memo, useEffect, } from 'react';
+import React, { memo, useEffect } from 'react';
 import 'react-native-reanimated';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StyleSheet, View } from 'react-native';
@@ -25,7 +25,6 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
   });
 
